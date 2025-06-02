8e69ab1f3d410c2eb2bfbc1fa154dbd1a445c173
nizamike229 <nizamike229@mail.ru>
2025-06-02 15:09:48 +0500

    Scaffolded DbContext

---
+++ TestCleanArch/SneakerShop.Infrastructure/Persistance/SneakerDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SneakerShop.Domain.Entities;
+
+namespace SneakerShop.Infrastructure.Persistance;
+
+public partial class SneakerDbContext : DbContext
+{
+    public SneakerDbContext()
+    {
+    }
+
+    public SneakerDbContext(DbContextOptions<SneakerDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Sneaker> Sneakers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Data Source=../../main.sqlite");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Sneaker>(entity =>
+        {
+            entity.ToTable("sneakers");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Description).HasColumnName("description");
+            entity.Property(e => e.Price)
+                .HasColumnType("INT")
+                .HasColumnName("price");
+            entity.Property(e => e.Title)
+                .HasColumnType("text(255)")
+                .HasColumnName("title");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
