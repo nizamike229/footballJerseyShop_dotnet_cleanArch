138fcd80f2dd6dcefabc88b78c54c17de94d2c63
nizamike229 <nizamike229@mail.ru>
2025-06-02 15:11:05 +0500

    Implemented middleware to catch all exceptions

---
+++ TestCleanArch/SneakerShop.Web/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,46 @@
+using System.Net;
+using System.Text.Json;
+
+namespace SneakersShop.Middleware;
+
+public class ExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+    public ExceptionHandlerMiddleware(
+        RequestDelegate next,
+        ILogger<ExceptionHandlerMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+    
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unhandled exception occurred");
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        var response = new
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = "Internal Server Error",
+            Details = exception.Message
+        };
+
+        return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+    }
+}
