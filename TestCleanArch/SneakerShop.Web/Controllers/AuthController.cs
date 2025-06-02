4fcd5a98fe4ab3b5fac8f1ca699e46d54ce11d3e
nizamike229 <nizamike229@mail.ru>
2025-06-02 16:29:23 +0500

    Implemented Auth endpoints

---
+++ TestCleanArch/SneakerShop.Web/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using SneakerShop.Application.Services;
+using SneakersShop.Mapping;
+using SneakersShop.Models.Requests;
+
+namespace SneakersShop.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+    private readonly IConfiguration _configuration;
+
+    public AuthController(IAuthService authService, IConfiguration configuration)
+    {
+        _authService = authService;
+        _configuration = configuration;
+    }
+
+    [HttpPost]
+    [ActionName("login")]
+    public async Task<ActionResult<string>> Login([FromBody] UserRequest request)
+    {
+        var token = await _authService.Login(request.ToUser());
+        Response.Cookies.Append("access_token", token, new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.Strict,
+            Expires = DateTime.UtcNow.AddMinutes(
+                Convert.ToDouble(_configuration["Jwt:ExpireMinutes"]))
+        });
+        return Ok(new { Message = "Login successful" });
+    }
+
+    [HttpPost]
+    [ActionName("register")]
+    public async Task<ActionResult<string>> Register([FromBody] UserRequest request)
+    {
+        return Ok(new { Message = await _authService.Register(request.ToUser()) });
+    }
+
+    [HttpPost]
+    [ActionName("logout")]
+    public ActionResult<string> Logout()
+    {
+        Response.Cookies.Delete("access_token");
+        return Ok(new { Message = "Logout successful" });
+    }
+}
