fc2ce59d286d0dd94a19cf833c988fe15d6aa9e0
nizamike229 <nizamike229@mail.ru>
2025-06-02 15:09:59 +0500

    Added model according to DB

---
+++ TestCleanArch/SneakerShop.Domain/Entities/Sneaker.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SneakerShop.Domain.Entities;
+
+public class Sneaker
+{
+    [MaxLength(255)]
+    public required string Id { get; set; }
+
+    [MaxLength(255)]
+    public required string Title { get; set; }
+
+    [MaxLength(500)]
+    public required string Description { get; set; }
+
+    public required int Price { get; set; }
+}
